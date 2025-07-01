ec303ccd6359b6c55ef693284ccb2cae9c77e59c
kaberege <kabgnestor@gmail.com>
2025-07-01 17:29:56 +0200

    added rank properties

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -3,6 +3,12 @@ SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
 GROUP BY user_id;
 
+-- Rank properties based on number of bookings using RANK()
+SELECT property_id, COUNT(*) AS total_bookings,
+       RANK() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id;
+
 -- Ranking properties by number of bookings
 SELECT property_id, COUNT(*) AS total_bookings,
        ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS row_num
